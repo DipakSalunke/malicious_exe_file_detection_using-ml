93b2b50c4bdeed7aba13d0e3151241debdc23f12
Dipak Salunke <dipakbs810@gmail.com>
2020-09-28 02:43:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def upload_file():
     ##########################################
     # Load classifier
     clf = joblib.load(os.path.join(os.path.dirname(os.path.realpath(__file__)),'classifier/classifier.pkl'))
-    features =pickle.loads(open('/app/classifier/features.pkl'),'r').read()
+    features =pickle.loads(open('/app/classifier/features.pkl','r'))
      ##########################################
      #tweet = request.form['tweet']
      #tweet=cutit(f.filename, 12)
